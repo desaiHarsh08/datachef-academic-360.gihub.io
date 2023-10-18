6b6a1050da8f38042671a6c42a6491ab08c4d2d3
harshNileshDesai <harsh.desai@kasisto.com>
2023-10-18 13:32:01 +0530

    fifth  commit

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(cors());
 app.use(bodyParser.json({ limit: '5000mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10000000000mb' }));
 app.use(bodyParser.text({ type: 'text/*', limit: '10000000000mb' }));
